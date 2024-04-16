be2c3ad340020c3f40fcbb272974cda039254704
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-04-16 15:54:07 +0300

    Update dcrtpoly.cpp

--- src/dcrtpoly.cpp
+++ src/dcrtpoly.cpp
@@ -2,5 +2,7 @@
 #include "openfhe.h"
 
 void wrap_DCRTPolyImpl(jlcxx::Module& mod) {
-  mod.add_type<lbcrypto::DCRTPolyImpl>("DCRTPolyImpl");
+  mod.add_type<jlcxx::Parametric<jlcxx::TypeVar<1>>>("DCRTPolyImpl",
+      jlcxx::julia_base_type<lbcrypto::BigVector>())<lbcrypto::DCRTPolyImpl>;
+    .apply<lbcrypto::DCRTPolyImpl<lbcrypto::BigVector>>([](auto wrapped) {});
 }
